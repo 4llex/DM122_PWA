154bf71dcae391f5dddfc6cc5b36e7aee96fdf71
Alex Rosa <alexr@gec.inatel.br>
2020-09-19 15:29:42 -0300

    segundo commit

--- sw.js
+++ sw.js
@@ -1,17 +1,39 @@
+const cacheName = 'app-shell-v1';
+
+const assetsToCache = [
+    'offline.html'
+];
+
+async function cacheStaticAssets() {
+    const cache = await caches.open(cacheName);
+    return cache.addAll(assetsToCache);
+}
+  
+
 self.addEventListener('install', event => {
     console.log('[Service Worker] Installing service worker...');
+    event.waitUntil(cacheStaticAssets());
     self.skipWaiting();
-  });
+});
+  
   
 
 self.addEventListener('activate', event => {
     console.log('[Service Worker] Activating service worker...');
     return self.clients.claim();
 });
-  
+
+async function networkFirst(request) {
+    try {
+      return await fetch(request);
+    } catch (error) {
+      const cache = await caches.open(cacheName);
+      return cache.match('offline.html');
+    }
+}
   
 self.addEventListener('fetch', event => {
     console.log('[Service Worker] Fetch event...', event);
-    event.respondWith(fetch(event.request));
-  });
+    event.respondWith(networkFirst(event.request));
+});
   
